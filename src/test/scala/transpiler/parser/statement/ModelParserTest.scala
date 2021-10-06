aa8ae9b53bee03b08b893cf12f0768ebbae61952
Michael <michael.jhaywoods@gmail.com>
2021-10-06 23:50:04 +0100

    Added model parser tests.

--- src/test/scala/transpiler/parser/statement/ModelParserTest.scala
+++ src/test/scala/transpiler/parser/statement/ModelParserTest.scala
@@ -27,26 +27,32 @@ class ModelParserTest extends AnyFunSpec with Matchers {
 
     it("Should parse a model that extends a parent") {
       val code =
-        """class Test extends ParentClass
-          |  let exampleMethod(): Int = 1
+        """class Test extends ParentClass {
+          |  let exampleMethod() Int = { 1 }
+          |}
         """.stripMargin.replace("\r", "")
-      // TestUtil.parse(code, StatementParser.statementParser) shouldBe ClassModel(Name("Test"), List(), List(), Some(Type(RefLocal(Name("ParentClass")))), List(), ArrayBuffer(), ArrayBuffer(Method(Name("exampleMethod"), List(), ArrayBuffer(), ArrayBuffer(), Some(Type(RefLocal(Name("Int")))), Inline(IntConst(1)))))
+      val Parsed.Success(value, _) = parse(code, StatementParser.statementParser(_))
+     value shouldBe ClassModel(Name("Test"),List(),List(),Some(Type(RefLocal(Name("ParentClass")))),List(),ArrayBuffer(),ArrayBuffer(Method(Name("exampleMethod"),List(),ArrayBuffer(),ArrayBuffer(),Some(Type(RefLocal(Name("Int")))),CurlyBracketsBlock(ArrayBuffer(ExprAsStmt(IntConst(1)))))))
     }
 
     it("Should parse a model that extends a parent and implements a trait") {
       val code =
-        """class Test extends ParentClass with Trait
-          |  let exampleMethod(): Int = 1
+        """class Test extends ParentClass with Trait {
+          |  let exampleMethod() Int = { 1 }
+          |}
         """.stripMargin.replace("\r", "")
-      // TestUtil.parse(code, StatementParser.statementParser) shouldBe ClassModel(Name("Test"), List(), List(), Some(Type(RefLocal(Name("ParentClass")))), List(), ArrayBuffer(Type(RefLocal(Name("Trait")))), ArrayBuffer(Method(Name("exampleMethod"), List(), ArrayBuffer(), ArrayBuffer(), Some(Type(RefLocal(Name("Int")))), Inline(IntConst(1)))))
+      val Parsed.Success(value, _) = parse(code, StatementParser.statementParser(_))
+     value shouldBe ClassModel(Name("Test"),List(),List(),Some(Type(RefLocal(Name("ParentClass")))),List(),ArrayBuffer(Type(RefLocal(Name("Trait")))),ArrayBuffer(Method(Name("exampleMethod"),List(),ArrayBuffer(),ArrayBuffer(),Some(Type(RefLocal(Name("Int")))),CurlyBracketsBlock(ArrayBuffer(ExprAsStmt(IntConst(1)))))))
     }
 
     it("Should parse a model that extends a parent and implements multiple traits") {
       val code =
-        """class Test extends ParentClass with Trait1 with Trait2 with Trait3
-          |  let exampleMethod(): Int = 1
+        """class Test extends ParentClass with Trait1 with Trait2 with Trait3 {
+          |  let exampleMethod() Int = { 1 }
+          |}
         """.stripMargin.replace("\r", "")
-      // TestUtil.parse(code, StatementParser.statementParser) shouldBe ClassModel(Name("Test"), List(), List(), Some(Type(RefLocal(Name("ParentClass")))), List(), ArrayBuffer(Type(RefLocal(Name("Trait1"))), Type(RefLocal(Name("Trait2"))), Type(RefLocal(Name("Trait3")))), ArrayBuffer(Method(Name("exampleMethod"), List(), ArrayBuffer(), ArrayBuffer(), Some(Type(RefLocal(Name("Int")))), Inline(IntConst(1)))))
+      val Parsed.Success(value, _) = parse(code, StatementParser.statementParser(_))
+     value shouldBe ClassModel(Name("Test"),List(),List(),Some(Type(RefLocal(Name("ParentClass")))),List(),ArrayBuffer(Type(RefLocal(Name("Trait1"))), Type(RefLocal(Name("Trait2"))), Type(RefLocal(Name("Trait3")))),ArrayBuffer(Method(Name("exampleMethod"),List(),ArrayBuffer(),ArrayBuffer(),Some(Type(RefLocal(Name("Int")))),CurlyBracketsBlock(ArrayBuffer(ExprAsStmt(IntConst(1)))))))
     }
   }
 }
