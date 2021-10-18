4f43b5bc69feab49399c7ddff8506a8ed6265232
Michael2109 <michael.jhaywoods@gmail.com>
2021-10-18 14:38:26 +0100

    Improved test.

--- src/test/scala/transpiler/codegen/StatementCodeGenTest.scala
+++ src/test/scala/transpiler/codegen/StatementCodeGenTest.scala
@@ -1,23 +1,15 @@
 package transpiler.codegen
 
-import fastparse.{Parsed, parse}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.must.Matchers
-import transpiler.parser.StatementParser
-import jdk.nashorn.internal.parser.Parser
-import jdk.nashorn.internal.runtime.Context
-import jdk.nashorn.internal.runtime.ErrorManager
-import jdk.nashorn.internal.runtime.options.Options
-import jdk.nashorn.internal.runtime.Source
-import transpiler.js.{AST2JS, ModelJS}
-import transpiler.utils.JavascriptBeautifier
+import transpiler.utils.{CompileUtils, JavaScriptCompare}
 
 
 class StatementCodeGenTest extends AnyFunSpec with Matchers {
   describe("Model parser") {
     it("Should parse a model with no fields") {
       val code =
-        """package x.y.zS
+        """package x.y.z
           |class ClassName {
           |
           |  let variable: Int = 1000
@@ -38,32 +30,35 @@ class StatementCodeGenTest extends AnyFunSpec with Matchers {
           |}
         """.stripMargin.replace("\r", "")
 
-      val Parsed.Success(ast, _) = parse(code, StatementParser.moduleParser(_))
+      val compiledCode: String = CompileUtils.compileCode(code)
 
-      println(ast)
-      // Process AST
-      val modelIRs: Seq[ModelJS] = AST2JS.moduleToIR(ast).models
+      println(compiledCode)
 
-      println(modelIRs)
+      val expectedResult: String =
+        """package x.y.z;
+          |class ClassName {
+          |    variable() {
+          |        return 1000;
+          |    }
+          |    x() {
+          |        var y = 10;
+          |        var array = [];
+          |        array.forEach(i => {
+          |            if (y < 5) {
+          |                console.log("Something");
+          |            } else {
+          |                if (false) {
+          |                    console.log("Else");
+          |                } else {
+          |                    console.log(false);
+          |                }
+          |            }
+          |        });
+          |    }
+          |}
+        """.stripMargin.replace("\r", "")
 
-      val compiledCode: List[String] = modelIRs.map(StatementCodeGen.modelGenCode).toList
-      println(compiledCode.head)
-
-      val options = new Options("nashorn");
-      options.set("anon.functions", true);
-      options.set("parse.only", true);
-      options.set("scripting", true);
-      options.set("language", "es6")
-
-      val errors = new ErrorManager();
-      val context = new Context(options, errors, Thread.currentThread().getContextClassLoader());
-      val source   =  Source.sourceFor("test", compiledCode.head);
-      val parser = new Parser(context.getEnv(), source, errors);
-      val functionNode = parser.parse();
-      val block = functionNode.getBody();
-      val statements = block.getStatements();
-
-      println(JavascriptBeautifier.beautify(compiledCode.head))
+      JavaScriptCompare.checkMatch(expectedResult, compiledCode)
     }
 
   }
